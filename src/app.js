d2ef50aca56c2791622d2d6f817d8c20f74b6f77
David Kwon <david.kwon@clover.com>
2024-11-13 14:05:32 -0700

    Create users router in routes/users.js and export to app.js

--- src/app.js
+++ src/app.js
@@ -3,4 +3,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
 
+const usersRouter = require("../routes/users");
+
+app.use("/users", usersRouter);
+
 module.exports = app;
