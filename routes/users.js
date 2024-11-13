71531140482373e9a33e19ca379f64bd2067844a
David Kwon <david.kwon@clover.com>
2024-11-13 14:10:01 -0700

    Create route to GET all users in routes/users.js

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const usersRouter = express.Router();
+const { User } = require("../models");
 
-
+usersRouter.get("/", async (req, res, next) => {
+    try {
+        const allUsers = await User.findAll();
+        res.json(allUsers);
+    } catch {
+        next(error);
+    }
+})
 
 module.exports = usersRouter;
