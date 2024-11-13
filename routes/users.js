c3fa7983b1dfe69965b8c04d98fd211a09d7564f
David Kwon <david.kwon@clover.com>
2024-11-13 14:30:54 -0700

    Create route to GET all shows watched by a user

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const usersRouter = express.Router();
-const { User } = require("../models");
+const { User, Show } = require("../models");
 
 usersRouter.get("/", async (req, res, next) => {
     try {
@@ -14,6 +14,18 @@ usersRouter.get("/", async (req, res, next) => {
 usersRouter.get("/:id", async (req, res, next) => {
     try {
         const targetUser = await User.findByPk(req.params.id);
+        res.json(targetUser);
+    } catch(error) {
+        next(error)
+    }
+})
+
+usersRouter.get("/:id/shows", async (req, res, next) => {
+    try {
+        const targetUser = await User.findOne({
+            where: { id: req.params.id },
+            include: Show
+        });
         res.json(targetUser);
     } catch(error) {
         next(error)
