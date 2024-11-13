7dd403b3896bccc3e0a2cbc71466de89047b64bd
David Kwon <david.kwon@clover.com>
2024-11-13 14:11:53 -0700

    Create route to GET one user by id

--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,17 @@ usersRouter.get("/", async (req, res, next) => {
     try {
         const allUsers = await User.findAll();
         res.json(allUsers);
-    } catch {
+    } catch(error) {
         next(error);
+    }
+})
+
+usersRouter.get("/:id", async (req, res, next) => {
+    try {
+        const targetUser = await User.findByPk(req.params.id);
+        res.json(targetUser);
+    } catch(error) {
+        next(error)
     }
 })
 
