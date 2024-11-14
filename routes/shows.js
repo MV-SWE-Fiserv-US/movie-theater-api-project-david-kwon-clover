14b41ba485c133542ff7f9528f67f783a6d1bf10
David Kwon <david.kwon@clover.com>
2024-11-14 08:44:02 -0700

    Create route to POST a show with server side validation

--- routes/shows.js
+++ routes/shows.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const showsRouter = express.Router();
 const { User, Show } = require("../models"); 
+const { check, validationResult } = require("express-validator");
 
 showsRouter.get("/", async (req, res, next) => {
     try {
@@ -52,6 +53,18 @@ showsRouter.put("/:id/available", async (req, res, next) => {
         res.status(200).json(updatedShow);
     } catch {
         next(error);
+    }
+})
+
+showsRouter.post("/", [
+    check("title").isLength({ max: 25 }).withMessage("title must be 25 characters or less")
+], async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        res.json(422).json({error: errors.array()});
+    } else {
+        const newShow = await Show.create(req.body);
+        res.status(200).json({created: newShow});
     }
 })
 
